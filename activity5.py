8e7eb2b45947b59347ed438ecb589f90d89c05ea
arjhayricafranca1-svg <arjhayricafranca1@gmail.com>
2026-08-30 18:16:40 +0800

    Add files via upload
    School purposes only

--- activity5.py
+++ activity5.py
@@ -1,3 +1,4 @@
+
 #operators
 # Arithmetic 
 # + - Addition 
@@ -18,12 +19,21 @@ sum = a + b
 difference = b - a
 product = e * d
 div = a / c 
+floor_div = e // a
+expo = d ** e
+mod = b % c
 
 
 print("The sum of ",a," and ",b," is ",sum)
 print("The difference of ",a," and",b," is ",difference)
 print("The product of ",e," and",d," is ",product)
 print("The div of ",a," and",c," is ",a/c)
+print("The floor_div of ",e," and",a," is ",e//a)
+print("The expo of ",d," and",e," is ",d**e)
+print("The mod of ",b," and",c," is ",b%c)
+
+
+
 
 
 
