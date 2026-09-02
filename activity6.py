4dc0385b174fde234e8a0de3aca59d1da9df11e6
arjhayricafranca1-svg <arjhayricafranca1@gmail.com>
2026-09-02 22:37:00 +0800

    Add files via upload
    for school purposes only

---
+++ activity6.py
@@ -0,0 +1,18 @@
+# fist step to dynamic content 
+
+
+name = input("What is your name .? ")
+print("Hi", name," How are you doing ? ")
+input("?.")
+print("Thats good")
+
+age = input("What is your age .?")
+print("Oh! youre", age," years old ")
+
+bdate = input("When is your bdate .?")
+print("Nice", bdate," same " )
+
+#print("Hi", name," How are you doing ? ")
+#print("Oh! youre", age," years old ")
+#print("Nice", bdate," same " )
+
