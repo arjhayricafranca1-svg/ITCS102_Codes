ebaec6deeca465a6f5e6fbb95e429743b5346ff0
arjhayricafranca1-svg <arjhayricafranca1@gmail.com>
2026-09-08 18:43:15 +0800

    Add files via upload

--- activity8.py
+++ activity8.py
@@ -2,4 +2,14 @@ hobbies = " " #blank string
 
 hb = input("What are your hobbies ? ")
 
-hobbies= +=
+hobbies += hb + ", "
+
+hb = input("What else ? ")
+
+hobbies += hb + ", "
+
+hb = input("What else ? ")
+
+hobbies += hb + ", "
+
+print("My hobbies are ", hobbies )
